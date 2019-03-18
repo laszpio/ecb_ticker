f3f2dc3b8df89bd4a9209e6ae1ca273893bd550f
Piotr Laszewski <laszpio@gmail.com>
2019-03-18 11:24:26 +0100

    Fetch XML envelope content when parsing the current response.

--- lib/ticker.ex
+++ lib/ticker.ex
@@ -9,7 +9,8 @@ defmodule Ticker do
     case HTTPotion.get(@endpoint) do
       %HTTPotion.Response{body: body, status_code: 200} ->
         case body |> parse_response_body do
-          {:ok, response} -> process_response(response)
+          {:ok, response} -> process_response_data(response)
+          {:error, message} -> message
         end
 
       %HTTPotion.ErrorResponse{message: message} ->
@@ -18,16 +19,18 @@ defmodule Ticker do
   end
 
   defp parse_response_body(response) do
-    {:ok, XmlToMap.naive_map(response)}
+    case XmlToMap.naive_map(response) do
+      %{"{http://www.gesmes.org/xml/2002-08-01}Envelope" => data} -> {:ok, data}
+      {:error, message} -> {:error, message}
+    end
   end
 
-  defp process_response(response) do
-    [tree | _] = response |> Map.values()
-    tree = tree |> Map.get("Cube") |> Map.get("Cube")
+  defp process_response_data(data) do
+    data = data |> Map.get("Cube") |> Map.get("Cube")
 
     %{
-      date: tree["time"],
-      rates: Enum.map(tree["Cube"], fn r -> {r["currency"], r["rate"] |> String.to_float()} end)
+      date: data["time"],
+      rates: Enum.map(data["Cube"], fn r -> {r["currency"], r["rate"] |> String.to_float()} end)
     }
   end
 end
