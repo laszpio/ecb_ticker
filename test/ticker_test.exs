cb40c690f61a416d8cca31bafe5703a1e85843e8
Piotr Laszewski <laszpio@gmail.com>
2019-03-20 21:10:38 +0100

    Test parsing response XML’s data container

--- test/ticker_test.exs
+++ test/ticker_test.exs
@@ -1,4 +1,13 @@
 defmodule TickerTest do
   use ExUnit.Case
   doctest Ticker
+
+  import Ticker
+
+  test "returns response XML's envelope" do
+    envelope = File.read!("test/eurofxref-envelope.xml")
+    {:ok, response} = parse_response_body(envelope)
+
+    assert response |> Map.get("Cube") == "data container"
+  end
 end
