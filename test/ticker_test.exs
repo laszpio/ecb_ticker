78428272cd58c42d30d6d642f4913b94c1f803d3
Piotr Laszewski <laszpio@gmail.com>
2019-03-20 21:26:37 +0100

    Ticker: test rates extraction for daily feed

--- test/ticker_test.exs
+++ test/ticker_test.exs
@@ -10,4 +10,23 @@ defmodule TickerTest do
 
     assert response |> Map.get("Cube") == "data container"
   end
+
+  @daily %{
+    "time" => "2019-03-20",
+    "Cube" => [
+      %{"currency" => "USD", "rate" => "1.1354"},
+      %{"currency" => "IDR", "rate" => "16082"}
+    ]
+  }
+
+  test "extracts rates' date" do
+    assert extract_rates(@daily) |> Map.get(:date) == "2019-03-20"
+  end
+
+  test "extracts rates for all currencies" do
+    assert extract_rates(@daily) |> Map.get(:rates) == [
+             {"USD", 1.1354},
+             {"IDR", 16082.0}
+           ]
+  end
 end
