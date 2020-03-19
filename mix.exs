dcdbc742514666ee7ca2f6c20f11c6cd1b5cc7fc
Piotr Laszewski <laszpio@gmail.com>
2020-03-19 10:43:01 +0100

    Mix: change httpotion to tesla.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Ticker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpotion, "~> 3.1.0"},
+      {:tesla, "~> 1.3.0"},
       {:jason, "~> 1.0"},
       {:elixir_xml_to_map, "~> 0.1"},
       {:exvcr, "~> 0.11.0", only: :test},
