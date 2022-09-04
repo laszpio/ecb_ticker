6acfc474687ae82999088c1f071fb54067741e6a
Piotr Laszewski <laszpio@gmail.com>
2022-09-04 11:11:20 +0100

    Update VCR cassetes

--- test/ticker_test.exs
+++ test/ticker_test.exs
@@ -110,7 +110,7 @@ defmodule TickerTest do
       result = daily()
 
       assert is_map(result)
-      assert result[:date] == ~D[2022-03-08]
+      assert result[:date] == ~D[2022-09-02]
 
       rates = result[:rates]
       assert is_list(rates)
@@ -133,7 +133,7 @@ defmodule TickerTest do
       result = historical()
 
       assert is_list(result)
-      assert length(result) == 64
+      assert length(result) == 65
 
       Enum.each(result, fn daily ->
         assert daily |> Map.has_key?(:date)
